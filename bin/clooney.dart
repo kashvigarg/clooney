9905ccb0dd9e7b33cbcc92fbae8c5159e7a56474
kashvigarg <kashvigarg02@gmail.com>
2023-01-03 12:23:22 +0530

    added test docs

--- bin/clooney.dart
+++ bin/clooney.dart
@@ -1,5 +1,28 @@
-import 'package:clooney/clooney.dart' as clooney;
+// import 'package:clooney/clooney.dart' as clooney;
+
+// void main(List<String> arguments) {
+//   print('${clooney.name()}!');
+// }
+
+import 'dart:convert';
+import 'dart:io';
+
+import 'package:args/args.dart';
 
 void main(List<String> arguments) {
-  print('${clooney.name()}!');
+  stdout.write("Enter file name");
+  String? path = stdin.readLineSync();
+
+  stdout.write("Enter coding file name");
+  String? workingPath = stdin.readLineSync();
+
+  String finalWorkingPath = workingPath ?? "";
+  String finalPath = path ?? "";
+  var file = File(finalPath);
+  var workFile = File(finalWorkingPath);
+
+  file.writeAsString("Omg, I did it!", mode: FileMode.append);
+
+  // enter file w coding path, append its contents to the final path file w heading source code
+  // enter .exe data there, how?
 }
